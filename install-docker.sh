01e269b0a23d673d5cca472c7da7598d2dfb74c7
Kevin Klein <kevin.klein@cgi.com>
2019-01-24 20:53:22 +0100

    simple install

--- install-docker.sh
+++ install-docker.sh
@@ -6,8 +6,9 @@ sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
-sudo apt-get update
+sudo apt update
+sudo apt -y upgrade
 #curl https://raw.githubusercontent.com/rancher/install-docker/master/18.09.1.sh | sh
-sudo apt-get install -y docker-ce
+sudo apt install -y docker-ce
 #sudo apt-get update
 #sudo apt-get install -y docker-ce
