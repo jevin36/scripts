1ad1177b0632193edd0843b839bba95e591a772a
Kevin Klein <kevin.klein@cgi.com>
2019-01-24 19:51:53 +0100

    install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-lsb_release -cs
-add-apt-repository \
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo lsb_release -cs
+sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
-apt-get update
+sudo apt-get update
 #curl https://raw.githubusercontent.com/rancher/install-docker/master/18.09.1.sh | sh
-apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 #sudo apt-get update
 #sudo apt-get install -y docker-ce
